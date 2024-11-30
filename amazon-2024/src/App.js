bb2dbfcfec5b64a58c601aec172977a4bd4fd2a2
esima1 <mokemebratu@gmail.com>
2024-11-30 14:45:51 -0600

    category

--- amazon-2024/src/App.js
+++ amazon-2024/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import CarouselEffect from "./Components/Carousel/Carousel";
 import Header from "./Components/Header/Header";
-import { Carousel } from "react-responsive-carousel";
+import Category from "./Components/Category/Category";
 
 function App() {
   return (
     <div className="">
       <Header />
       <CarouselEffect />
+      <Category />
     </div>
   );
 }
---
+++ amazon-2024/src/Components/Category/categoryFullInfos.js
@@ -0,0 +1,26 @@
+export const categoryInfos = [
+  {
+    name: "CPU",
+    title: "Electronics",
+    imgLink:
+      "https://images.unsplash.com/photo-1591799265444-d66432b91588?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3Ryb25pY3N8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Jewels",
+    title: "Jewelery",
+    imgLink:
+      "https://plus.unsplash.com/premium_photo-1674255466849-b23fc5f5d3eb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8amV3ZWxlcnl8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    name: "Iran",
+    title: "Men's clothing",
+    imgLink:
+      "https://images.unsplash.com/photo-1578238591955-f360d5eda965?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVuJ3MlMjBjbG90aGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    name: "Azul",
+    title: "Women's clothing",
+    imgLink:
+      "https://images.unsplash.com/photo-1608169941368-fd7ac986ac3d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
