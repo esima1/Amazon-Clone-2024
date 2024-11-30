9166f51a663fae56b53e7b638d30bc67f0119842
esima1 <mokemebratu@gmail.com>
2024-11-30 12:03:10 -0600

    carousel

--- amazon-2024/src/App.js
+++ amazon-2024/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import CarouselEffect from "./Components/Carousel/Carousel";
 import Header from "./Components/Header/Header";
+import { Carousel } from "react-responsive-carousel";
 
 function App() {
   return (
     <div className="">
       <Header />
+      <CarouselEffect />
     </div>
   );
 }
