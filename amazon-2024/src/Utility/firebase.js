af043c6e6a2a1deeb9a822b3dec12aa16491922d
esima1 <mokemebratu@gmail.com>
2025-01-01 20:31:45 -0600

    env&other

--- amazon-2024/src/Utility/firebase.js
+++ amazon-2024/src/Utility/firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "firebase/app";
+//auth
+import { getAuth } from "firebase/auth";
 
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Process.env.REACT_APP_apikey,
+  authDomain: Process.env.REACT_APP_authDomain,
+  projectId: Process.env.REACT_APP_projectId,
+  storageBucket: Process.env.REACT_APP_storageBucket,
+  messagingSenderId: Process.env.REACT_APP_messagingSenderId,
+  appId: Process.env.REACT_APP_appId,
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = app.firestore();
